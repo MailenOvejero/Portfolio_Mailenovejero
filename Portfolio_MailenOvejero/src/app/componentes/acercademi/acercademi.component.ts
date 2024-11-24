3af0c365606e1dbc8c57e5f0f4450e8c94a83227
MailenOvejero <yamilamailenovejero27@gmail.com>
2024-11-24 02:26:22 -0300

    Creacion de componentes

---
+++ Portfolio_MailenOvejero/src/app/componentes/acercademi/acercademi.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-acercademi',
+  imports: [],
+  templateUrl: './acercademi.component.html',
+  styleUrl: './acercademi.component.css'
+})
+export class AcercademiComponent {
+
+}
---
+++ Portfolio_MailenOvejero/src/app/componentes/hys/hys.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hys',
+  imports: [],
+  templateUrl: './hys.component.html',
+  styleUrl: './hys.component.css'
+})
+export class HysComponent {
+
+}
